f4d262451415ad6db48688dc90e3e5ba4c1c3e3a
Robert J <Rjsf06@gmail.com>
2022-05-31 23:00:31 +0000

    intervalo aula 3

--- app.py
+++ app.py
@@ -25,4 +25,11 @@ def exibir_entradas():
     entradas = []
     for titulo, texto, criado_em in cur.fetchall():
         entradas.append({"titulo": titulo, "texto": texto, "criado_em": criado_em})
-    return render_template("hello.html", entradas=entradas)
+    return render_template("exibir_entradas.html", entradas=entradas)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/logout")
+    def logout
