94c00e2b4f0450e150abab6cf1323fe7b324fe41
Robert J <Rjsf06@gmail.com>
2022-06-01 01:18:38 +0000

    projeto finalizado

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, flash, session, redirect, url_for, abort
 import sqlite3
 
 DATABASE = "banco.bd"
-SECRETY_KEY = "chave"
+SECRET_KEY = "chave"
 
 app = Flask("Hello")
 app.config.from_object(__name__)
@@ -27,9 +27,31 @@ def exibir_entradas():
         entradas.append({"titulo": titulo, "texto": texto, "criado_em": criado_em})
     return render_template("exibir_entradas.html", entradas=entradas)
 
-@app.route("/login")
+@app.route('/inserir', methods=["POST"])
+def inserir_entradas():
+    if not session.get('logado'):
+        abort(401)
+    titulo = request.form['titulo']
+    texto = request.form['texto']
+    sql = "INSERT INTO entradas(titulo, texto) VALUES (?, ?)"
+    g.bd.execute(sql, [titulo, texto]) 
+    g.bd.commit()  
+    flash("Nova entrada gravada com Sucesso!")
+    return redirect(url_for("exibir_entradas"))
+
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    erro = None
+    if request.method == "POST":
+        if request.form['username'] == "admin" and request.form['password'] == "admin":
+            session['logado'] = True
+            flash("Login efetuado!")
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou Senha inválidos"
+    return render_template("login.html", erro=erro)
 
 @app.route("/logout")
-    def logout
+def logout():
+    session.pop('logado', None)
+    flash("Logout Efetuado com Sucesso!")
+    return redirect(url_for("exibir_entradas"))
